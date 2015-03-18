9a5b28a560b6554c7e734f8c5e8b7ca9ad026fa8
Nick Walker <nick@nickwalker.us>
2015-03-18 12:26:00 -0600

    Fix 2014 reel overlays

--- app/views/cinematographer.blade.php
+++ app/views/cinematographer.blade.php
@@ -5,7 +5,7 @@
 	<figure>
 		<h3>Narrative</h3>
 		<ul class="thumbnails reels">
-			<li><div class="new-content"><a href="https://www.youtube.com/watch?v=Wii6Kp-s9WU" data-year="coming soon" class="resolution-2k sixteen-by-nine"><img src="/img/reels/2014.jpg" /></a></div></li>
+			<li><div class="new-content"><a href="https://www.youtube.com/watch?v=Wii6Kp-s9WU" data-year="2014" class="resolution-2k sixteen-by-nine" style="pointer-events: none;"><img src="/img/reels/2014.jpg" /></a></div></li>
 			<li><a href="https://vimeo.com/46161651" data-year="2011"><img src="/img/reels/2011.jpg" /></a></li>
 			<li><a href="https://vimeo.com/18381848" data-year="2010"><img src="/img/reels/2010.jpg" /></a></li>
 			<li><a class="fancybox.iframe" href="http://www.youtube.com/embed?listType=playlist&list=PLsYARrhGsN4siTZoWw_Yq5JF6pNVsBTbm&origin=http://alexwalker.co/" data-year="Completed Works Playlist" class="not-fancy"><img src="/img/reels/playlist.jpg" /></a></li>
