3c1a6d1524143286ac0ebf90ec7767e0e08a4289
Nick Walker <nick@nickwalker.us>
2015-10-02 22:18:20 -0500

    Add scroll header blur
    Add header reel link
    Remove some dangling resource links
    Close #4
    Close #6

---
+++ js/scrollblur.js
@@ -0,0 +1,98 @@
+/**
+ * Created by Sallar Kaboli <sallar.kaboli@gmail.com>
+ * @sallar
+ *
+ * Released under the MIT License.
+ * http://sallar.mit-license.org/
+ *
+ * This document demonstrates three things:
+ *
+ * - Creating a simple parallax effect on the content
+ * - Creating a Medium.com-style blur on scroll image
+ * - Getting scroll position using requestAnimationFrame for better performance
+ */
+
+
+/**
+ * Cache
+ */
+var $blur = $('#slider-container')
+  , wHeight  = $(window).height();
+
+$(window).on('resize', function(){
+  wHeight = $(window).height();
+});
+
+/**
+ * requestAnimationFrame Shim
+ */
+window.requestAnimFrame = (function()
+{
+  return  window.requestAnimationFrame       ||
+          window.webkitRequestAnimationFrame ||
+          window.mozRequestAnimationFrame    ||
+          function( callback ){
+            window.setTimeout(callback, 1000 / 60);
+          };
+})();
+
+/**
+ * Scroller
+ */
+function Scroller()
+{
+  this.latestKnownScrollY = 0;
+  this.ticking            = false;
+}
+
+Scroller.prototype = {
+  /**
+   * Initialize
+   */
+  init: function() {
+    window.addEventListener('scroll', this.onScroll.bind(this), false);
+  },
+
+  /**
+   * Capture Scroll
+   */
+  onScroll: function() {
+    this.latestKnownScrollY = window.scrollY;
+    this.requestTick();
+  },
+
+  /**
+   * Request a Tick
+   */
+  requestTick: function() {
+    if( !this.ticking ) {
+      window.requestAnimFrame(this.update.bind(this));
+    }
+    this.ticking = true;
+  },
+
+  /**
+   * Update.
+   */
+  update: function() {
+    var currentScrollY = this.latestKnownScrollY;
+    this.ticking       = false;
+
+    /**
+     * Do The Dirty Work Here
+     */
+    var percentage = currentScrollY / wHeight;
+    var opacity = percentage - 0.3;
+    var blurRadius = Math.max((percentage - 0.3)  * 25, 0);
+    $blur.css({
+      'opacity' : 1.0 - opacity,
+      '-webkit-filter' : 'blur('+blurRadius + 'px)'
+    });
+  }
+};
+
+/**
+ * Attach!
+ */
+var scroller = new Scroller();
+scroller.init();
