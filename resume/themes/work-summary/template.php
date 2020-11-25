32e1c7ae0d8de939475e5c535f45906ebffe1b3a
Alex Walker <alexanderiwalker@icloud.com>
2020-11-25 11:50:16 -0600

    Update template.php

--- resume/themes/work-summary/template.php
+++ resume/themes/work-summary/template.php
@@ -87,5 +87,22 @@
 		</footer>
 
 	</div>
+			<!-- Default Statcounter code for Portfolio Website
+http://alexwalker.co -->
+<script type="text/javascript">
+var sc_project=12435982; 
+var sc_invisible=1; 
+var sc_security="dcc5fdba"; 
+</script>
+<script type="text/javascript"
+src="https://www.statcounter.com/counter/counter.js"
+async></script>
+<noscript><div class="statcounter"><a title="Web Analytics
+Made Easy - StatCounter" href="https://statcounter.com/"
+target="_blank"><img class="statcounter"
+src="https://c.statcounter.com/12435982/0/dcc5fdba/1/"
+alt="Web Analytics Made Easy -
+StatCounter"></a></div></noscript>
+<!-- End of Statcounter Code -->
 </body>
 </html>
